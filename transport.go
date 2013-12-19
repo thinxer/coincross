6b95d2c55fa4b974e17c5dccded332a69710f733
Jianfei Wang <me@thinxer.com>
2013-12-19 13:58:05 +0800

    transport: change to total timeout

--- transport.go
+++ transport.go
@@ -6,19 +6,20 @@ import (
 	"time"
 )
 
-func TimeoutDialer(connectTimeout time.Duration, readWriteTimeout time.Duration) func(net, addr string) (c net.Conn, err error) {
+func TimeoutDialer(connectTimeout time.Duration, totalTimeout time.Duration) func(net, addr string) (c net.Conn, err error) {
 	return func(netw, addr string) (net.Conn, error) {
+		start := time.Now()
 		conn, err := net.DialTimeout(netw, addr, connectTimeout)
 		if err != nil {
 			return nil, err
 		}
-		conn.SetDeadline(time.Now().Add(readWriteTimeout))
+		conn.SetDeadline(start.Add(totalTimeout))
 		return conn, nil
 	}
 }
 
-func TimeoutTransport(connectTimeout time.Duration, readWriteTimeout time.Duration) *http.Transport {
+func TimeoutTransport(connectTimeout time.Duration, totalTimeout time.Duration) *http.Transport {
 	return &http.Transport{
-		Dial: TimeoutDialer(connectTimeout, readWriteTimeout),
+		Dial: TimeoutDialer(connectTimeout, totalTimeout),
 	}
 }
