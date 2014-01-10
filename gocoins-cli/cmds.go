d865de6509166bae7dce7a460a17a1b59b5c74ad
Jianfei Wang <me@thinxer.com>
2014-01-10 16:44:42 +0800

    rewrite with go-commander

--- gocoins-cli/cmds.go
+++ gocoins-cli/cmds.go
@@ -6,120 +6,127 @@ import (
 	"os"
 	"runtime"
 	"strconv"
+	"code.google.com/p/go-commander"
 
 	s "github.com/thinxer/gocoins"
 )
 
-func must(v interface{}, err error) interface{} {
-	if err != nil {
-		panic(err)
-	}
-	return v
-}
-
-func balance(c s.Client) {
-	b, err := c.Balance()
-	check(err)
-	for k, v := range b {
-		fmt.Printf("%v:%v\n", k, v)
+func init() {
+	cmd := newCmd("balance", "")
+	cmd.Run = func(cmd *commander.Command, args []string) {
+		b, err := client.Balance()
+		check(err)
+		for k, v := range b {
+			fmt.Printf("%v:%v\n", k, v)
+		}
 	}
 }
 
-func trade(c s.Client, tradeType s.TradeType) {
-	price := must(strconv.ParseFloat(flag.Arg(1), 64)).(float64)
-	amount := must(strconv.ParseFloat(flag.Arg(2), 64)).(float64)
-	id, err := c.Trade(tradeType, *flagPair, price, amount)
+func trade(c s.Client, tradeType s.TradeType, args []string) {
+	price := must(strconv.ParseFloat(args[0], 64)).(float64)
+	amount := must(strconv.ParseFloat(args[1], 64)).(float64)
+	id, err := client.Trade(tradeType, flagPair, price, amount)
 	check(err)
 	fmt.Println(id)
 }
 
-func buy(c s.Client) {
-	trade(c, s.Buy)
-}
-
-func sell(c s.Client) {
-	trade(c, s.Sell)
-}
-
-func orders(c s.Client) {
-	orders, err := c.Orders()
-	check(err)
-	for _, o := range orders {
-		fmt.Println(o)
+func init() {
+	cmd := newCmd("buy", "price amount")
+	cmd.Run = func(cmd *commander.Command, args []string) {
+		fmt.Println(args)
+		trade(client, s.Buy, args)
 	}
-
-}
-
-func cancel(c s.Client) {
-	orderId := must(strconv.ParseInt(flag.Arg(1), 10, 64)).(int64)
-	ok, err := c.Cancel(orderId)
-	check(err)
-	fmt.Println(ok)
-}
-
-func transactions(c s.Client) {
-	var limit int64 = 50
-	if flag.NArg() > 1 {
-		limit = must(strconv.ParseInt(flag.Arg(1), 10, 64)).(int64)
-	}
-	tr, err := c.Transactions(int(limit))
-	check(err)
-	for _, t := range tr {
-		fmt.Println(t)
-	}
-}
-
-func history(c s.Client) {
-	trades, _, err := c.History(*flagPair, -1)
-	check(err)
-	for _, t := range trades {
-		fmt.Println(t)
-	}
-}
-
-func orderbook(c s.Client) {
-	limit := 50
-	if flag.NArg() > 1 {
-		limit, _ = strconv.Atoi(flag.Arg(1))
-	}
-	orders, err := c.Orderbook(*flagPair, limit)
-	check(err)
-	fmt.Println("Asks:")
-	for _, o := range orders.Asks {
-		fmt.Printf("%v\t%v\n", o.Price, o.Amount)
-	}
-	fmt.Println("Bids:")
-	for _, o := range orders.Bids {
-		fmt.Printf("%v\t%v\n", o.Price, o.Amount)
-	}
-}
-
-func watch(c s.Client) {
-	ct := make(chan s.Trade)
-	go c.Stream(*flagPair, -1, ct)
-	for t := range ct {
-		fmt.Println(t)
-	}
-}
-
-func ticker(c s.Client) {
-	ticker, err := c.Ticker(*flagPair)
-	check(err)
-	fmt.Printf("%+v\n", ticker)
 }
 
 func init() {
-	cmds["balance"] = balance
-	cmds["buy"] = buy
-	cmds["sell"] = sell
-	cmds["orders"] = orders
-	cmds["cancel"] = cancel
-	cmds["transactions"] = transactions
+	cmd := newCmd("sell", "price amount")
+	cmd.Run = func(cmd *commander.Command, args []string) {
+		trade(client, s.Sell, args)
+	}
+}
 
-	cmds["history"] = history
-	cmds["orderbook"] = orderbook
-	cmds["watch"] = watch
-	cmds["ticker"] = ticker
+func init() {
+	cmd := newCmd("orders", "")
+	cmd.Run = func(cmd *commander.Command, args []string) {
+		orders, err := client.Orders()
+		check(err)
+		for _, o := range orders {
+			fmt.Println(o)
+		}
+	}
+}
+
+func init() {
+	cmd := newCmd("cancel", "orderid")
+	cmd.Run = func(cmd *commander.Command, args []string) {
+		orderId := must(strconv.ParseInt(args[0], 10, 64)).(int64)
+		ok, err := client.Cancel(orderId)
+		check(err)
+		fmt.Println(ok)
+	}
+}
+
+func init() {
+	cmd := newCmd("transactions", "[-limit 50]")
+	limit := (&cmd.Flag).Int("limit", 50, "")
+	cmd.Run = func(cmd *commander.Command, args []string) {
+		tr, err := client.Transactions(*limit)
+		check(err)
+		for _, t := range tr {
+			fmt.Println(t)
+		}
+	}
+}
+
+func init() {
+	cmd := newCmd("history", "[-since=-1]")
+	since := (&cmd.Flag).Int64("since", -1, "")
+	cmd.Run = func(cmd *commander.Command, args []string) {
+		trades, _, err := client.History(flagPair, *since)
+		check(err)
+		for _, t := range trades {
+			fmt.Println(t)
+		}
+
+	}
+}
+
+func init() {
+	cmd := newCmd("orderbook", "[-limit 50]")
+	limit := flag.Int("limit", 50, "")
+	cmd.Run = func(cmd *commander.Command, args []string) {
+		orders, err := client.Orderbook(flagPair, *limit)
+		check(err)
+		fmt.Println("Asks:")
+		for _, o := range orders.Asks {
+			fmt.Printf("%v\t%v\n", o.Price, o.Amount)
+		}
+		fmt.Println("Bids:")
+		for _, o := range orders.Bids {
+			fmt.Printf("%v\t%v\n", o.Price, o.Amount)
+		}
+
+	}
+}
+
+func init() {
+	cmd := newCmd("watch", "")
+	cmd.Run = func(cmd *commander.Command, args []string) {
+		ct := make(chan s.Trade)
+		go client.Stream(flagPair, -1, ct)
+		for t := range ct {
+			fmt.Println(t)
+		}
+	}
+}
+
+func init() {
+	cmd := newCmd("ticker", "")
+	cmd.Run = func(cmd *commander.Command, args []string) {
+		ticker, err := client.Ticker(flagPair)
+		check(err)
+		fmt.Printf("%+v\n", ticker)
+	}
 }
 
 func check(err error) {
@@ -128,4 +135,11 @@ func check(err error) {
 		fmt.Fprintf(os.Stderr, "Error: %v [%s:%d]\n", err, file, line)
 		os.Exit(2)
 	}
+}
+
+func must(v interface{}, err error) interface{} {
+	if err != nil {
+		panic(err)
+	}
+	return v
 }
--- gocoins-cli/main.go
+++ gocoins-cli/main.go
@@ -1,40 +1,59 @@
-// A simple tool for trading on the command line interface.
+// A simple tool for trading on CLI.
 package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"time"
 
+	commander "code.google.com/p/go-commander"
 	s "github.com/thinxer/gocoins"
 	_ "github.com/thinxer/gocoins/all"
 )
 
-var (
-	flagPair    = &s.Pair{s.CNY, s.BTC}
-	flagTimeout = flag.Duration("timeout", 10*time.Second, "timeout for connections")
+// Populate this slice with newCmd, and it will be used in the commander.
+var cmds []*commander.Command
 
-	cmds = make(map[string]func(c s.Client))
-)
-
-func init() {
-	flag.Var(flagPair, "pair", "pair to operate on")
+func newCmd(name, short string) *commander.Command {
+	cmd := &commander.Command{
+		UsageLine: name + " " + short,
+		Flag:      *flag.NewFlagSet(name, flag.ExitOnError),
+		Short:     short,
+	}
+	cmds = append(cmds, cmd)
+	return cmd
 }
 
+var (
+	flagPair    = s.Pair{s.CNY, s.BTC}
+	flagTimeout time.Duration
+	client      s.Client
+)
+
 func main() {
-	flag.Parse()
+	// Construct the commander
+	cmd := commander.Commander{
+		Name:     os.Args[0],
+		Commands: cmds,
+		Flag:     flag.NewFlagSet("gocoins client", flag.ExitOnError),
+	}
+	cmd.Flag.Var(&flagPair, "pair", "pair to operate on")
+	cmd.Flag.DurationVar(&flagTimeout, "timeout", 10*time.Second, "timeout for connections")
+	if err := cmd.Flag.Parse(os.Args[1:]); err != nil {
+		panic(err)
+	}
+
+	// Construct the client
 	exchange := os.Getenv("EXCHANGE")
 	apikey := os.Getenv("APIKEY")
 	secret := os.Getenv("SECRET")
-	client := s.New(exchange, apikey, secret, s.TimeoutTransport(*flagTimeout, *flagTimeout))
+	client = s.New(exchange, apikey, secret, s.TimeoutTransport(flagTimeout, flagTimeout))
+	if client == nil {
+		panic("create client failed")
+	}
 
-	cmd := flag.Arg(0)
-	fn, ok := cmds[cmd]
-	if ok {
-		fn(client)
-	} else {
-		fmt.Fprintf(os.Stderr, "not supported: %s\n", cmd)
-		os.Exit(1)
+	// Actually run the commands
+	if err := cmd.Run(cmd.Flag.Args()); err != nil {
+		return
 	}
 }
